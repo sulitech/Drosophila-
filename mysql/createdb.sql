3850232fb93d2efc8addd081c9cfb257e095d04f
seungheui ryu <30386988+sulitech@users.noreply.github.com>
2023-05-24 10:03:12 +0900

    Update createdb.sql

--- mysql/createdb.sql
+++ mysql/createdb.sql
@@ -1,1 +1,15 @@
+--create database
 CREATE DATABASE drosphila DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+-- create user root/admin
+
+--data import to mysql
+
+mysql -uroot -padmin --default-character-set=utf8 drosphila < gene.sql
+mysql -uroot -padmin --default-character-set=utf8 drosphila < cell.sql
+mysql -uroot -padmin --default-character-set=utf8 drosphila < motifregulon.sql
+mysql -uroot -padmin --default-character-set=utf8 drosphila < motifregulonauc.sql
+mysql -uroot -padmin --default-character-set=utf8 drosphila < tissue.sql
+mysql -uroot -padmin --default-character-set=utf8 drosphila < tissue_cell.sql
+mysql -uroot -padmin --default-character-set=utf8 drosphila < tissue_cell_gene.sql
+mysql -uroot -padmin --default-character-set=utf8 drosphila < tissue_gene.sql
