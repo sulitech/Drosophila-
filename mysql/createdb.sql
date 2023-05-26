91e30281c41eca911e0100a955f4c6b6383ca186
seungheui ryu <30386988+sulitech@users.noreply.github.com>
2023-05-26 12:01:01 +0900

    Update createdb.sql
    Update createdb.sql

--- mysql/createdb.sql
+++ mysql/createdb.sql
@@ -1,16 +1,16 @@
 --create database
 CREATE DATABASE drosphila DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
--- create user root/admin
-create user 'root'@'localhost' identified by 'admin';
+-- create user myuser/userpassord
+create user 'myuser'@'localhost' identified by 'userpassord';
 
 --data import to mysql
 
-mysql -uroot -padmin --default-character-set=utf8 drosphila < gene.sql
-mysql -uroot -padmin --default-character-set=utf8 drosphila < cell.sql
-mysql -uroot -padmin --default-character-set=utf8 drosphila < motifregulon.sql
-mysql -uroot -padmin --default-character-set=utf8 drosphila < motifregulonauc.sql
-mysql -uroot -padmin --default-character-set=utf8 drosphila < tissue.sql
-mysql -uroot -padmin --default-character-set=utf8 drosphila < tissue_cell.sql
-mysql -uroot -padmin --default-character-set=utf8 drosphila < tissue_cell_gene.sql
-mysql -uroot -padmin --default-character-set=utf8 drosphila < tissue_gene.sql
+mysql -umyuser -puserpassord --default-character-set=utf8 drosphila < gene.sql
+mysql -umyuser -puserpassord --default-character-set=utf8 drosphila < cell.sql
+mysql -umyuser -puserpassord --default-character-set=utf8 drosphila < motifregulon.sql
+mysql -umyuser -puserpassord --default-character-set=utf8 drosphila < motifregulonauc.sql
+mysql -umyuser -puserpassord --default-character-set=utf8 drosphila < tissue.sql
+mysql -umyuser -puserpassord --default-character-set=utf8 drosphila < tissue_cell.sql
+mysql -umyuser -puserpassord --default-character-set=utf8 drosphila < tissue_cell_gene.sql
+mysql -umyuser -puserpassord --default-character-set=utf8 drosphila < tissue_gene.sql
